c919811c37f43184e5789ef75de4473195c65ee6
sxzh93 <shaoxioz@uber.com>
2018-07-17 10:43:08 -0700

    add license comments

--- internal/loadbalancebenchmarklauncher/main.go
+++ internal/loadbalancebenchmarklauncher/main.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2018 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package main
 
 import (
@@ -10,8 +30,8 @@ func main() {
 	config := &lbbench.TestConfig{
 		ClientGroup: []lbbench.ClientGroup{
 			{
-				Cnt:    100,
-				Rps:    100,
+				Cnt:    1000,
+				Rps:    1000,
 				LType:  lbbench.FewestPending,
 				LUType: lbbench.Static,
 			},
@@ -20,12 +40,12 @@ func main() {
 			{
 				Cnt:           8,
 				Type:          lbbench.NormalMachine,
-				LatencyConfig: lbbench.RpsLatency(100),
+				LatencyConfig: lbbench.RpsLatency(2000000),
 			},
 			{
-				Cnt:           1,
+				Cnt:           2,
 				Type:          lbbench.SlowMachine,
-				LatencyConfig: lbbench.RpsLatency(1),
+				LatencyConfig: lbbench.RpsLatency(5),
 			},
 		},
 		Duration: 3 * time.Second,
